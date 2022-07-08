f2892975572c150c3323ef2494b1872dcf24c26b
BekovaDeniza <bekova_02@inbox.ru>
2022-07-08 16:35:45 +0300

    fixed comment

--- task2.py
+++ task2.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 animals = {}
 
 
@@ -15,26 +14,22 @@ def get_animals(url):
         soup = BeautifulSoup(page, 'lxml')
         uls = soup.find('div', class_='mw-category mw-category-columns').find_all('ul')
         for ul in uls:
-            names = ul.find_all('a') #Парсим названия животных
+            names = ul.find_all('a')  # Парсим названия животных
             for name in names:
-                if name.get('title')[0] in animals: #Если в словаре есть первая буква животного, добавляем 1
+                if name.get('title')[0] in animals:  # если буква присутствует в словаре, прибавляем 1
                     animals[name.get('title')[0]] += 1
-                else: #иначе добавляем букву в словарь
+                else:  # иначе добавляем букву в словарь
                     animals[name.get('title')[0]] = 1
         links = soup.find('div', id='mw-pages').find_all('a')
-        for a in links: #проверяем есть ли ссылка на следующую страницу
-            if a.text == 'Следующая страница':
+        for a in links:
+            if a.text == 'Следующая страница':  # проверяем есть ли ссылка на следующую страницу
                 url = 'https://ru.wikipedia.org/' + a.get('href')
                 page = requests.get(url).text
                 fl = False
-        if fl: break # если ссылки не было, выходим из цикла
+        if fl: break  # если ссылки не было, выходим из цикла
 
 
 if __name__ == "__main__":
     get_animals("https://inlnk.ru/jElywR")
     for i in animals:
         print(i, ':', animals[i])
-
-
-
-
